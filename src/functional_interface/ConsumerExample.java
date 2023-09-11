325b09369cefac761e4dc6e44fa9d1085f6977e3
samuel <samugit.projects@gmail.com>
2023-09-11 12:39:46 -0300

    Functional Interface - Predicate<T>

--- src/functional_interface/ConsumerExample.java
+++ src/functional_interface/ConsumerExample.java
@@ -24,10 +24,8 @@ public class ConsumerExample {
         };
 
         // Usar o Consumer para imprimir números pares no Stream
-        numeros.forEach(n -> {
-            if (n % 2 == 0) {
-                System.out.println(n);
-            }
-        });
+        numeros.stream()
+                .filter(n -> n % 2 == 0)
+                .forEach(System.out::println);
     }
 }
