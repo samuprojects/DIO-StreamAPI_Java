c79c11ddf53633da0ddb5ed4706cbeaad5ac7b76
samuel <samugit.projects@gmail.com>
2023-09-14 12:54:53 -0300

    Desafio 9 - Verificar se todos os números da lista são distintos

---
+++ src/stream_api/Desafio09.java
@@ -0,0 +1,31 @@
+package stream_api;
+
+import java.util.Arrays;
+import java.util.List;
+
+@SuppressWarnings("ALL")
+public class Desafio09 {
+
+    /*
+    * Desafio 9 - Verificar se todos os números da lista são distintos (não se repetem):
+
+Com a Stream API, verifique se todos os números da lista são distintos (não se repetem) e exiba o resultado no console.
+
+    *
+    * */
+
+    public static void main(String[] args) {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        boolean numerosNaoRepetidos = numeros.stream()
+                .allMatch(numero -> numeros.stream()
+                        .filter(num -> num.equals(numero))
+                        .count() == 1);
+        if (numerosNaoRepetidos){
+            System.out.println("Nenhum número está repetido");
+        } else {
+            System.out.println("Existem números repetidos");
+        }
+    }
+}
